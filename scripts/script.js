0531586e44629cb5e33c63c6421daec45248e87e
Полина <Shumilina246@gmail.com>
2021-07-06 16:52:33 +0300

    fixed: scroll

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,39 @@ const headerCityButton = document.querySelector('.header__city-button');
 //     headerCityButton.textContent = localStorage.getItem('lomoda-location');
 // }
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?'
+headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
 
 headerCityButton.addEventListener('click', () => {
     const city = prompt('Укажите ваш город');
     headerCityButton.textContent = city;
     localStorage.setItem('lomoda-location', city);
 });
+
+//Блокировка скролла
+
+const disableScroll = () => {
+    const widthScroll = window.innerWidth - document.body.offsetWidth;
+    // создаем новое свойство у объекта, чтобы сохзранить текущее положение страницы
+
+    document.body.dbScrollY = window.scrollY;
+
+    document.body.style.cssText = `
+        position: fixed;
+        top: ${-window.scrollY}px;
+        left: 0;
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        padding-right: ${widthScroll}px;
+    `
+};
+
+const enableScroll = () => {
+    document.body.style.cssText = '';
+    window.scroll({
+        top: document.body.dbScrollY
+    })
+};
 
 //Модальное окно
 
@@ -19,10 +45,12 @@ const cartOverlay = document.querySelector('.cart-overlay');
 
 const cartModalOpen = () => {
     cartOverlay.classList.add('cart-overlay-open');
+    disableScroll();
 };
 
 const cartModalClose = () => {
     cartOverlay.classList.remove('cart-overlay-open');
+    enableScroll();
 };
 
 subHeaderCart.addEventListener('click', cartModalOpen);
@@ -32,6 +60,6 @@ cartOverlay.addEventListener('click', event => {
 
     if (target.classList.contains('cart__btn-close') || target.matches('.cart-overlay')) {
         cartModalClose();
-    } 
+    }
     // является ли крестиком элемент, по которому кликнули
 });
