8866d16476f126bf5ca2092c3b62c0e28c04d89a
Полина <Shumilina246@gmail.com>
2021-07-07 15:12:28 +0300

    create: getData

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,27 @@
-const headerCityButton = document.querySelector('.header__city-button');
+const headerCityButton = document.querySelector(".header__city-button");
 
 // if (localStorage.getItem('lomoda-location')) {
 //     headerCityButton.textContent = localStorage.getItem('lomoda-location');
 // }
 
-headerCityButton.textContent = localStorage.getItem('lomoda-location') || 'Ваш город?';
+headerCityButton.textContent =
+  localStorage.getItem("lomoda-location") || "Ваш город?";
 
-headerCityButton.addEventListener('click', () => {
-    const city = prompt('Укажите ваш город');
-    headerCityButton.textContent = city;
-    localStorage.setItem('lomoda-location', city);
+headerCityButton.addEventListener("click", () => {
+  const city = prompt("Укажите ваш город");
+  headerCityButton.textContent = city;
+  localStorage.setItem("lomoda-location", city);
 });
 
 //Блокировка скролла
 
 const disableScroll = () => {
-    const widthScroll = window.innerWidth - document.body.offsetWidth;
-    // создаем новое свойство у объекта, чтобы сохзранить текущее положение страницы
+  const widthScroll = window.innerWidth - document.body.offsetWidth;
+  // создаем новое свойство у объекта, чтобы сохзранить текущее положение страницы
 
-    document.body.dbScrollY = window.scrollY;
+  document.body.dbScrollY = window.scrollY;
 
-    document.body.style.cssText = `
+  document.body.style.cssText = `
         position: fixed;
         top: ${-window.scrollY}px;
         left: 0;
@@ -28,38 +29,67 @@ const disableScroll = () => {
         height: 100vh;
         overflow: hidden;
         padding-right: ${widthScroll}px;
-    `
+    `;
 };
 
 const enableScroll = () => {
-    document.body.style.cssText = '';
-    window.scroll({
-        top: document.body.dbScrollY
-    })
+  document.body.style.cssText = "";
+  window.scroll({
+    top: document.body.dbScrollY,
+  });
 };
 
 //Модальное окно
 
-const subHeaderCart = document.querySelector('.subheader__cart');
-const cartOverlay = document.querySelector('.cart-overlay');
+const subHeaderCart = document.querySelector(".subheader__cart");
+const cartOverlay = document.querySelector(".cart-overlay");
 
 const cartModalOpen = () => {
-    cartOverlay.classList.add('cart-overlay-open');
-    disableScroll();
+  cartOverlay.classList.add("cart-overlay-open");
+  disableScroll();
 };
 
 const cartModalClose = () => {
-    cartOverlay.classList.remove('cart-overlay-open');
-    enableScroll();
+  cartOverlay.classList.remove("cart-overlay-open");
+  enableScroll();
 };
 
-subHeaderCart.addEventListener('click', cartModalOpen);
 
-cartOverlay.addEventListener('click', event => {
-    const target = event.target;
 
-    if (target.classList.contains('cart__btn-close') || target.matches('.cart-overlay')) {
-        cartModalClose();
-    }
-    // является ли крестиком элемент, по которому кликнули
+// Запрос базы данных
+
+const getData = async () => {
+  const data = await fetch("db.json");
+
+  if (data.ok) {
+    return data.json();
+  } else {
+    throw new Error(
+      `Данные не были получены, ошибка ${data.status} ${data.statusText}`
+    );
+  }
+};
+
+const getGoods = (callback) => {
+  getData()
+    .then((data) => {
+      callback(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+subHeaderCart.addEventListener("click", cartModalOpen);
+
+cartOverlay.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (
+    target.classList.contains("cart__btn-close") ||
+    target.matches(".cart-overlay")
+  ) {
+    cartModalClose();
+  }
+  // является ли крестиком элемент, по которому кликнули
 });
